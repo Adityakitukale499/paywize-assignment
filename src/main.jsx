5c18c1fee1afe3a4c18c6f38bedd9bbf9f20e3c5
Aditya Kitukale <adityakitukale4599@gmail.com>
2024-08-25 22:17:14 +0530

    added commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { StyleOptionsProvider } from "./context/StyleOptionsContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <StyleOptionsProvider>
+      <App />
+    </StyleOptionsProvider>
+  </StrictMode>
+);
